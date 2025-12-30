54f201613b8ec52a9c59e26aa4f2b19f3dd87f1f
jbassoRMIT <s4101667@student.rmit.edu.au>
2025-12-30 17:36:42 +1100

    User can sort the list of displayed pokemon by pressing a button

--- memoryCards/src/PokemonList.jsx
+++ memoryCards/src/PokemonList.jsx
@@ -8,6 +8,7 @@ export default function PokemonList(){
     const [size,setSize]=useState("");
     const [moreInfo,setMoreInfo]=useState(false);
     const [listIndex,setListIndex]=useState(0);
+    const [sorted,setSorted]=useState(false);
     
     useEffect(()=>{
         fetch(`https://pokeapi.co/api/v2/pokemon?limit=100`)
@@ -29,12 +30,30 @@ export default function PokemonList(){
         setDisplay(true);
         console.log(data);
         console.log(size);
+        console.log(pokeList);
+        console.log(pokeListSorted);
     }
 
     //write a function to filter pokemon based on index
     const pokeList=data.filter((pokemon,index)=> {
         return index<size;
     })
+
+    //write a sorting function for pokeList
+    const sortingFunction=function(a,b){
+        if(a.name>b.name){
+            return 1
+        }
+        else if (a.name<b.name){
+            return -1
+        }
+        else{
+            return 0
+        }
+    }
+    
+
+    const pokeListSorted=pokeList.toSorted(sortingFunction);
 
     const handlePokemonClick=function(n){
         setMoreInfo(true);
@@ -55,12 +74,28 @@ export default function PokemonList(){
         )
     }
 
+
+    const toggleSort=function(){
+        if(sorted){
+            setSorted(false);
+        }
+        else{
+            setSorted(true);
+        }
+    }
+
     const displayPokemon=function(){
+        let displayList=pokeList;
+
+        if(sorted){
+            displayList=pokeListSorted;
+        }
+        
         return(
             <div>
                 <h3>List of all pokemon</h3>
                 <ul>
-                    {pokeList.map((pokemon,index)=>{
+                    {displayList.map((pokemon,index)=>{
                         return(
                             <div>
                                 <li onClick={() => handlePokemonClick(index)}>{pokemon.name}</li>
@@ -69,6 +104,7 @@ export default function PokemonList(){
                         ) 
                     })}
                 </ul>
+                <button onClick={toggleSort}>toggle sort</button>
             </div>
         )
     }
